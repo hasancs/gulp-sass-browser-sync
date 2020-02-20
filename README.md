4ff05d9819009fb315ed224c9a5bb91e8ae3e36b
Mahmudul Hasan <mahmudulhasan.cs@gmail.com>
2020-02-21 05:23:15 +0600

    Add pug/jade for html.

---
+++ README.md
@@ -0,0 +1,20 @@
+gulp-sass-browser-sync
+=============================
+A starter project including full setup for GulpJS, PUG, SASS, AutoPrefixer &amp; BrowserSync
+
+## System Preparation
+To use this starter project, you'll need the following things installed on your machine.
+1. [NodeJS](http://nodejs.org) - use the installer.
+2. [GulpJS](https://github.com/gulpjs/gulp) - `$ npm install -g gulp`
+## Local Installation
+1. Clone this repo, or download it into a directory of your choice.
+2. Inside the directory, run `npm install`.
+
+## Usage
+**development mode**
+This will give you file watching, browser synchronisation, auto-rebuild, CSS injecting etc etc.
+```shell
+$ gulp
+```
+
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,21 @@
 // Load Gulp...
-var gulp        = require('gulp');
-var del         = require('del');
-var sass        = require('gulp-sass');
-var maps        = require('gulp-sourcemaps');
-var uglify      = require('gulp-uglify');
-var prefixer    = require('gulp-autoprefixer');
-var cleanCSS    = require('gulp-clean-css');
-var imagemin    = require('gulp-imagemin');
-var rename      = require('gulp-rename');
-var browserSync = require('browser-sync');
+const { src, dest, series, parallel, watch }    = require('gulp');
+const del         = require('del');
+const sass        = require('gulp-sass');
+const pug         = require('gulp-pug');
+const maps        = require('gulp-sourcemaps');
+const uglify      = require('gulp-uglify');
+const prefixer    = require('gulp-autoprefixer');
+const cleanCSS    = require('gulp-clean-css');
+const imagemin    = require('gulp-imagemin');
+const rename      = require('gulp-rename');
+const browserSync = require('browser-sync');
 
 
 // Paths
-var paths = {
+const paths = {
   src: {
-    html: './src/*.html',
+    html: './src/**/*.pug',
     css: './src/sass/**/*.sass',
     img: './src/img/**',
     js: './src/js/**/*.js'
@@ -24,10 +25,9 @@ var paths = {
     css:  './serve/css',
     img:  './serve/img',
     js: './serve/js'
-
   }
 }
-var serve    = './serve/';
+const serve    = './serve/';
 
 
 // clean dist
@@ -37,42 +37,45 @@ function clean() {
 
 // copy html to dist
 function html(){
-  return gulp.src(paths.src.html)
-    .pipe(gulp.dest(paths.dist.html));
+  return src(paths.src.html)
+    .pipe(pug({
+        pretty: true
+      }))
+    .pipe(dest(paths.dist.html));
 }
 
 // Compile SASS
 function css() {
-  return gulp.src(paths.src.css)
+  return src(paths.src.css)
     .pipe(maps.init())
     .pipe(sass({outputStyle: 'expanded'}))
     .pipe(prefixer(['last 4 versions', '> 1%', 'ie 8', 'ie 7'], {cascade: false}))
     .pipe(cleanCSS({level: 2}))
     .pipe(rename({suffix: '.min'}))
     .pipe(maps.write('./'))
-    .pipe(gulp.dest(paths.dist.css))
+    .pipe(dest(paths.dist.css))
     .pipe(browserSync.stream());
 }
 
 // Compress (JPEG, PNG, GIF, SVG, JPG)
 function images(){
-  return gulp.src(paths.src.img)
+  return src(paths.src.img)
     .pipe(imagemin([
       imagemin.gifsicle({interlaced: true}),
       imagemin.jpegtran({progressive: true}),
       imagemin.optipng({optimizationLevel: 5})
     ]))
-    .pipe(gulp.dest(paths.dist.img));
+    .pipe(dest(paths.dist.img));
 }
 
 // Minify JS
 function javascript() {
-  return gulp.src(paths.src.js)
+  return src(paths.src.js)
     .pipe(maps.init())
     .pipe(uglify())
     .pipe(rename({suffix: '.min'}))
     .pipe(maps.write('./'))
-    .pipe(gulp.dest(paths.dist.js))
+    .pipe(dest(paths.dist.js))
     .pipe(browserSync.stream());
 }
 
@@ -83,13 +86,12 @@ function server() {
     browser: 'chrome',
     notify: false
   });
-  gulp.watch(paths.src.css, css);
-  gulp.watch(paths.src.img, images);
-  gulp.watch(paths.src.js, javascript);
-  gulp.watch(paths.src.html, html).on('change', browserSync.reload);
+  watch(paths.src.css, css);
+  watch(paths.src.img, images);
+  watch(paths.src.js, javascript);
+  watch(paths.src.html, html).on('change', browserSync.reload);
 }
 
 
 // Define default task that can be called by just running `gulp` from cli
-var build = gulp.series(clean, gulp.parallel(html, css, images, javascript, server));
-gulp.task('default', build);
+exports.default = series(clean, parallel(html, css, images, javascript), server)
